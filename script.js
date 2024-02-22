7a460a910fea3c891ca426f7833e85533b24d58e
Vinicius Gutierri <viniciusgutierri007@gmail.com>
2024-02-22 16:23:35 -0300

    adicionando filtro com base nas categorias

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ async function buscarEMostrarVideos() {
                         <img class="img-canal" src="${video.imagem}" alt="Logo do canal">
                         <h3 class="titulo-video">${video.titulo}</h3>
                         <p class="titulo-canal">${video.descricao}</p>
+                        <p class="categoria" hidden>${video.categoria}</p>
                     </div>
                 </li>
             `;
@@ -38,8 +39,26 @@ function filtrarPesquisa() {
     const valorFiltro = barraPesquisa.value.toLowerCase();
   
     videos.forEach((video) => {
-      const titulo = video.querySelector('.titulo-video').textContent.toLowerCase();
+        const titulo = video.querySelector('.titulo-video').textContent.toLowerCase();
   
-      video.style.display = valorFiltro ? titulo.includes(valorFiltro) ? 'block' : 'none' : 'block';
+        video.style.display = valorFiltro ? titulo.includes(valorFiltro) ? 'block' : 'none' : 'block';
     });
+}
+
+const botaoCategoria = document.querySelectorAll('.superior__item');
+
+botaoCategoria.forEach((botao) => {
+    let nomeCategoria = botao.getAttribute("name");
+    botao.addEventListener('click', () => filtrarPorCategoria(nomeCategoria));
+})
+
+function filtrarPorCategoria(filtro) {
+    const videos = document.querySelectorAll('.videos__item');
+
+    for (let video of videos) {
+        let categoria = video.querySelector('.categoria').textContent.toLocaleLowerCase();
+        let valorFiltro = filtro.toLocaleLowerCase();
+
+        video.style.display = !categoria.includes(valorFiltro) && valorFiltro != 'tudo' ? 'none' : 'block';
+    }
 }
